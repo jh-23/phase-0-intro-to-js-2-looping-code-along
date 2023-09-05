5723bee15c0bf12c0b0e3142467cdc06a0c2c12b
John Heger <heger7@gmail.com>
2023-09-05 18:22:44 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+function writeCards(names, event) {
+    let newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+const number = 10;
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
